7406587a12af95494460ba20a9adaab060fce2e7
josegarcia19809 <josegarcia19809@gmail.com>
2024-09-10 10:20:18 -0600

    Arreglos: for mejorado

---
+++ src/p01_arreglos/ArregloNumerosForMejorado.java
@@ -0,0 +1,11 @@
+package p01_arreglos;
+
+public class ArregloNumerosForMejorado {
+    public static void main(String[] args) {
+        int[] numeros = {3, 6, 9};
+
+        for (int valor : numeros) {
+            System.out.println(valor);
+        }
+    }
+}
---
+++ src/p01_arreglos/NombresForMejorado.java
@@ -0,0 +1,10 @@
+package p01_arreglos;
+
+public class NombresForMejorado {
+    public static void main(String[] args) {
+        String[] nombres = {"José", "María", "Rosa", "Ana", "Arturo"};
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
+}
