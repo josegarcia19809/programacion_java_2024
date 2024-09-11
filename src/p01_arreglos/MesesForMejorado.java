5c46149a10ed48347b3a91495e73e18358ef351b
josegarcia19809 <josegarcia19809@gmail.com>
2024-09-10 18:44:47 -0600

    Arreglos: for mejorado

---
+++ src/p01_arreglos/MesesForMejorado.java
@@ -0,0 +1,12 @@
+package p01_arreglos;
+
+public class MesesForMejorado {
+    public static void main(String[] args) {
+        String[] meses = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio",
+                "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+
+        for (String mes : meses) {
+            System.out.println(mes.toUpperCase());
+        }
+    }
+}
---
+++ src/p01_arreglos/diasForMejorado.java
@@ -0,0 +1,11 @@
+package p01_arreglos;
+
+public class diasForMejorado {
+    public static void main(String[] args) {
+        int[] diasDeLosMeses = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        for (int diasDeUnMes : diasDeLosMeses) {
+            System.out.print(diasDeUnMes + " ");
+        }
+    }
+}
