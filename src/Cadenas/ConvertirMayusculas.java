5cfe70a39a12f99dff4bef34d9210d93c37fb534
josegarcia19809 <josegarcia19809@gmail.com>
2025-03-29 07:07:59 -0600

    Cadenas: convertir a mayúsculas

---
+++ src/Cadenas/ConvertirMayusculas.java
@@ -0,0 +1,8 @@
+package Cadenas;
+
+public class ConvertirMayusculas {
+    public static void main(String[] args) {
+        String texto = "Enfrentar el cambio...";
+        System.out.println(texto.toUpperCase());
+    }
+}
